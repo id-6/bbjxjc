eeca60b5f9ddf1456e8a93562187e2974cbb5346
ابراهيم <143281628+id-6@users.noreply.github.com>
2023-11-17 20:14:13 +0300

    Add files via upload

---
+++ make/run.php
@@ -0,0 +1,42 @@
+<?php
+
+ob_start();
+//--------
+$token = "[*[TOKEN]*]"; # Token
+$IDBOT = "[*[IDBOT]*]";
+$userbot = "[*[USERBOT]*]";
+
+define('API_KEY',$token);
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+# Short
+$update = json_decode(file_get_contents("php://input"));
+$message = $update->message;
+$txt = $message->caption;
+$text = $message->text;
+$chat_id = $message->chat->id;
+$from_id = $message->from->id;
+$new = $message->new_chat_members;
+$message_id = $message->message_id;
+$type = $message->chat->type;
+$name = $message->from->first_name;
+
+$timesss=file_get_contents("times.txt");
+if($timesss=="✅"){
+
+require_once("../../test/runbots.php");
+
+}
+
